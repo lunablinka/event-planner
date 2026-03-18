2acd24c4862a758f1f0cdd7ddc06e6dabd04bece
Luna Vanaman <134232512+lunablinka@users.noreply.github.com>
2026-03-18 13:09:06 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,11 @@
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyBCP8AKvVUSkk-ikIuPYD69ve1tBi5KGHk",
-
   authDomain: "event-and-outreach-planner.firebaseapp.com",
-
   projectId: "event-and-outreach-planner",
-
   storageBucket: "event-and-outreach-planner.firebasestorage.app",
-
   messagingSenderId: "312330670583",
-
   appId: "1:312330670583:web:c7e705feec7c05090e9ef2"
-
-};
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
+}
